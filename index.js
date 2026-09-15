1a4a08e35d2c3675d68fd59b4212a2f0b3feaaad
Кугач Надежда <nadezdakugach@gmail.com>
2026-09-15 08:31:53 +0300

    Added student info and Pi calculation

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
 const http = require('http');
 
+function calculatePi(iterations) {
+  let sum = 0;
+  let sign = 1;
+  for (let i = 0; i < iterations; i++) {
+    sum += sign / (2 * i + 1);
+    sign = -sign;
+  }
+  return (sum * 4).toFixed(2);
+}
+
+const fio = 'Кугач Надежда Алексеевна';
+const group = '477';
+const journalNumber = 12;
+
+const pi = calculatePi(1000 + journalNumber * 100);
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/html; charset=utf-8' });
-  res.end('<h1>Привет, мир!</h1>');
+  res.end(`
+    <h1>Привет, мир!</h1>
+    <h1>${fio}</h1>
+    <h2>Группа: ${group}</h2>
+    <h2>Число Пи: ${pi}</h2>
+  `);
 });
 
 server.listen(3000, () => {
